ef519860c267ecc38eb52da00a5d179224aef604
vim12345 <vk2699945@gmail.com>
2024-02-04 04:27:38 +0530

    new

--- myapi/urls.py
+++ myapi/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myapi.api.views import SearchParagraphView, UserLoginView, UserRegistrationView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
